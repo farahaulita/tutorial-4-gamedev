fc9bf2f23933a99f0df7336dc62cbf999a3971a0
farah.sausan <farah.sausan@ui.ac.id>
2024-03-06 20:36:50 +0700

    tutorial 4 done

---
+++ Bee.gd
@@ -0,0 +1,12 @@
+extends RigidBody2D
+
+export var sceneName: String = "Level 1"
+
+
+
+
+func _on_Bee_body_entered(body):
+	if body.get_name() == "Player":
+		get_tree().change_scene(str("res://scenes/" + sceneName + ".tscn"))
+	else:
+		body.queue.free()
---
+++ Fish.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+export var sceneName: String = "Level 1"
+
+func _on_Fish_body_entered(body):
+	if body.get_name() == "Player":
+		get_tree().change_scene(str("res://scenes/" + sceneName + ".tscn"))
+	else:
+		body.queue.free()
+
+
+
+
+	
